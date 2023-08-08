2c8341ab180e23634e56beabf231c801d0328bb2
mattschneble <136631410+mattschneble@users.noreply.github.com>
2023-08-07 18:40:44 -0700

    updated Product model

--- models/Product.js
+++ models/Product.js
@@ -2,6 +2,8 @@
 const { Model, DataTypes } = require('sequelize');
 // import our database connection from config.js
 const sequelize = require('../config/connection');
+// import Category model (table) to use as foreign key
+const Category = require('./Category');
 
 // Initialize Product model (table) by extending off Sequelize's Model class
 class Product extends Model {}
@@ -9,7 +11,28 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    id:{
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_name:{
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isNumeric: true
+      }
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: { model: Category, key: "id"}
+    }
   },
   {
     sequelize,
