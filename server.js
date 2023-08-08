1f966f68596e1752edd1181492f2c311ca57dc74
mattschneble <136631410+mattschneble@users.noreply.github.com>
2023-08-08 08:44:10 -0700

    updated server.js

--- server.js
+++ server.js
@@ -1,16 +1,22 @@
+// Import express and routes
 const express = require('express');
 const routes = require('./routes');
-// import sequelize connection
+const sequelize = require('sequelize');
 
+// Import sequelize connection
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// Set up middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Turn on routes
 app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
-});
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}!`);
+  });
+})
