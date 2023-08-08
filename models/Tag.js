32baed72c4a4f0f29bb142137f570f396eff115c
mattschneble <136631410+mattschneble@users.noreply.github.com>
2023-08-07 18:55:13 -0700

    updated Tag model

--- models/Tag.js
+++ models/Tag.js
@@ -1,5 +1,6 @@
+// Import the Model class and DataTypes object from Sequelize
 const { Model, DataTypes } = require('sequelize');
-
+// Import the connection to the database from the config file
 const sequelize = require('../config/connection.js');
 
 class Tag extends Model {}
@@ -7,6 +8,15 @@ class Tag extends Model {}
 Tag.init(
   {
     // define columns
+    id:{
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    tag_name:{
+      type: DataTypes.STRING
+    },
   },
   {
     sequelize,
